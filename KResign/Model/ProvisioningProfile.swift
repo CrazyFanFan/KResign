827a70b31d8891c5e61bec69d1bcf7fd8cb456ac
Crazy凡 <827799383@qq.com>
2022-09-28 00:32:20 +0800

    Revert "Revert "Repalce ProvisioningProfile with MobileProvision""
    This reverts commit 0af2fbb5ded45c06a3421179196a72b97bf29d0e.

--- KResign/Model/ProvisioningProfile.swift
+++ KResign/Model/ProvisioningProfile.swift
@@ -6,105 +6,25 @@
 //
 
 import Foundation
+import MobileProvision
 
-struct ProvisioningProfile {
-    private(set) var name: String
-    private(set) var teamName: String
-    private(set) var isValid: Bool
-    private(set) var isDebug: Bool
-    private(set) var creationDate: Date
-    private(set) var expirationDate: Date
-    private(set) var UUID: String
-    private(set) var devices: [String]
-    private(set) var timeToLive: Int
-    private(set) var applicationIdentifier: String
-    private(set) var bundleIdentifier: String
-    private(set) var bundleIdentifierWithoutTeamID: String
-    private(set) var certificates: [Data]
-    private(set) var version: Int
-    private(set) var prefixes: [String]
-    private(set) var appIdName: String
-    private(set) var teamIdentifier: String
-    private(set) var path: URL
+private var displayCache = [String: String]()
 
-    private(set) var pickerDisplay: String
+typealias ProvisioningProfile = MobileProvision
+extension ProvisioningProfile {
+    var pickerDisplay: String {
+        if let display = displayCache[uuid] { return display }
 
-    init?(with fileURL: URL) {
-        var decoder: CMSDecoder?
-        var dataRef: CFData?
+        let display: String
 
-        CMSDecoderCreate(&decoder)
-
-        guard let tmpDecoder = decoder, let data = try? Data(contentsOf: fileURL) else { return nil }
-
-        let plist: Any? = data.withUnsafeBytes { (bufferRawBufferPointer) -> Any? in
-
-            let bufferPointer: UnsafePointer<UInt8> = bufferRawBufferPointer.baseAddress!.assumingMemoryBound(to: UInt8.self)
-            let rawPtr = UnsafeRawPointer(bufferPointer)
-            // USE THE rawPtr
-
-            CMSDecoderUpdateMessage(tmpDecoder, rawPtr, data.count)
-            CMSDecoderFinalizeMessage(tmpDecoder)
-            CMSDecoderCopyContent(tmpDecoder, &dataRef)
-            if let dataRef = dataRef,
-               let plist = try? PropertyListSerialization.propertyList(
-                from: dataRef as Data,
-                options: [.mutableContainers],
-                format: nil
-               ) {
-                return plist
-            }
-
-            return nil
+        if let id: String = applicationIdentifier {
+            display = "\(name) (\(id))"
+        } else {
+            display = name
         }
 
-        guard let plist = plist as? [String: AnyHashable] else { return nil }
+        displayCache[uuid] = display
 
-        func value<T>(for key: String) -> T? {
-            if let tmp = plist[key] as? T {
-                return tmp
-            }
-
-            assert(key == "ProvisionedDevices" || false, "Cannot read key: \(key)")
-            return nil
-        }
-
-        func entitlementsValue<T>(for key: String) -> T? {
-            if let tmp = plist["Entitlements"] as? [String: Any], let result = tmp[key] as? T {
-                return result
-            }
-            return nil
-        }
-
-        self.appIdName = value(for: "AppIDName") ?? ""
-        self.teamIdentifier = entitlementsValue(for: "com.apple.developer.team-identifier") ?? ""
-        self.name = value(for: "Name") ?? ""
-        self.teamName = value(for: "TeamName") ?? ""
-        self.isDebug = Int(1) == entitlementsValue(for: "get-task-allow")
-        self.creationDate = value(for: "CreationDate") ?? Date()
-        self.expirationDate = value(for: "ExpirationDate") ?? Date()
-        self.devices = value(for: "ProvisionedDevices") ?? []
-        self.timeToLive =  value(for: "TimeToLive") ?? 0
-        self.applicationIdentifier = entitlementsValue(for: "application-identifier") ?? ""
-        self.certificates = value(for: "DeveloperCertificates") ?? []
-        self.isValid = Date().timeIntervalSince(self.expirationDate) < 0
-        self.version = value(for: "Version") ?? 0
-        self.bundleIdentifier = self.applicationIdentifier
-        self.UUID = value(for: "UUID") ?? ""
-        self.prefixes = value(for: "ApplicationIdentifierPrefix") ?? []
-        self.path = fileURL
-        self.bundleIdentifierWithoutTeamID = self.bundleIdentifier
-            .replacingOccurrences(of: teamIdentifier + ".", with: "")
-
-        pickerDisplay = name + " (" + bundleIdentifierWithoutTeamID + ")"
-
-        decoder = nil
-        dataRef = nil
-    }
-}
-
-extension ProvisioningProfile: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(path)
+        return display
     }
 }
