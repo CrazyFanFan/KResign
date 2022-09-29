4eb5c675fff7d252a2a1f687122417f1ec6fa9c7
Crazy凡 <827799383@qq.com>
2022-09-29 23:25:41 +0800

    Regex

--- KResign/Model/Certificate.swift
+++ KResign/Model/Certificate.swift
@@ -14,9 +14,34 @@ struct Certificate: Hashable {
     var index: Int
     var sha1: String
     var name: String
-    var append: String?
+    //    var append: String?
 
     init?(string: String) {
+        var result: ParseResult?
+#if swift(>=5.7.1)
+        if #available(macOS 13.0, *) {
+            result = Self.parseInput(string: string)
+        } else {
+            result = Self.parseInputOld(string: string)
+        }
+#else
+        result = Self.parseInputOld(string: string)
+#endif
+        guard let result = result else {
+            return nil
+        }
+
+        index = result.index
+        sha1 = result.sha1
+        name = result.name
+        // append = result.append
+    }
+}
+
+private extension Certificate {
+    typealias ParseResult = (index: Int, sha1: String, name: String/*, append: String?*/)
+
+    static func parseInputOld(string: String) -> ParseResult? {
         precondition(regular != nil, "Certificate.regular should not be nil.")
 
         guard let regular = regular else {
@@ -37,14 +62,27 @@ struct Certificate: Hashable {
             return String(string[range])
         }
 
-        guard let index = getString(at: 1),
+        guard let indexString = getString(at: 1),
+              let index = Int(indexString),
               let sha1 = getString(at: 2),
               let name = getString(at: 3) else { return nil }
 
-        self.index = Int(index) ?? 0
-        self.sha1 = sha1
-        self.name = name
-
-        append = getString(at: 3)?.trimmingCharacters(in: .whitespacesAndNewlines)
+        return (index, sha1, name/*, getString(at: 4)?.trimmingCharacters(in: .whitespacesAndNewlines)*/)
     }
+
+#if swift(>=5.7.1)
+    @available(macOS 13.0, *)
+    static func parseInput(string: String) -> ParseResult? {
+        do {
+            guard let result = try /(\d+)\)\s([0-9A-Z]+)\s\"(.*)\"(.*)?/.firstMatch(in: string),
+                  let index = Int(result.1) else { return nil }
+
+            return (index, String(result.2), String(result.3)/*, result.4 == nil ? nil : String(result.4!)*/)
+        } catch {
+            NSLog("Certificate init match string failed. string: \(string)")
+            return nil
+        }
+    }
+#endif
+
 }
